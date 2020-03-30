a8154da5aa22901fb2d9b0a64aec8fb49f50748d
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-29 23:07:02 -0400

    Added picture

--- src/components/AboutMeMat/index.js
+++ src/components/AboutMeMat/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Grid, Paper, withStyles } from '@material-ui/core';
+import Picture from '../../images/Kenny.png'
 
 const styles = theme => ({
   paperPadding: {
     padding: "30px",
-    marginTop: "40px"
+    marginTop: "10px"
   },
+  picturePadding: {
+    marginTop: "40px"
+  }
 })
 
 function AboutMe(props){
   const { classes } = props
 
   return(
-    <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+    <Grid container direction="row" justify="center" alignItems="center">
+      <img src={Picture} className={classes.picturePadding} alt="Self Portrait" />
       <Paper elevation={3} className={classes.paperPadding}>
       Hello, my name is Kin-Wai Lam. I currently reside in Edison, NJ and I am a full stack developer. Welcome to my portfolio page, I am currently converting my portfolio into a full React experience. Please be patient with me while I learn React! 
       </Paper> 
