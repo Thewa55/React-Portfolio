0768384a0a511ac67d311b8876bb67071680ce11
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-30 10:07:45 -0400

    Updated about me

--- src/components/AboutMeMat/index.js
+++ src/components/AboutMeMat/index.js
@@ -19,8 +19,8 @@ function AboutMe(props){
     <Grid container direction="row" justify="center" alignItems="center">
       <img src={Picture} className={classes.picturePadding} alt="Self Portrait" />
       <Paper elevation={3} className={classes.paperPadding}>
-      Hello, my name is Kin-Wai Lam. I currently reside in Edison, NJ and I am a full stack developer. Welcome to my portfolio page, I am currently converting my portfolio into a full React experience. Please be patient with me while I learn React! 
-      </Paper> 
+        Hello, my name is Kin-Wai Lam. I am a full stack developer currently living in Edison, NJ. I've always had a passion in all things computer related, it first started when I was young and playing games on my first computer with floppy disks. In high school I was introduced to True Basic, PASCAL and Visual Basic. I went to college as a computer science major but ended up with an engineering degree. After graduating I worked at different jobs and after some time I decided that I need a career change. At the end of 2019 I enrolled into a bootcamp and now starting my journey into becoming a developer.
+      </Paper>
     </Grid>
   )
 }
